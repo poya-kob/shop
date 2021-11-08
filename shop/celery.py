045f6f9fce07e4bd2108fcc7c90b8a3038afd380
syml30 <y.mirkeshvari@gmail.com>
2021-11-08 17:13:33 +0330

    nginx

--- shop/celery.py
+++ shop/celery.py
@@ -4,5 +4,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
 
 app = Celery('shop')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.config:settings', namespace='CELERY')
 app.autodiscover_tasks()
